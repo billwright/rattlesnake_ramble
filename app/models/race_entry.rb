98ed92fe4426d13bbb9bbbce6b5c56cf59d96353
Bill Wright <bill@wwwright.com>
2017-07-30 20:59:16 +0000

    Updated race entry table to use Chronic Duration and integers and added some tests. Couldn't get email validation to work.

--- app/models/race_entry.rb
+++ app/models/race_entry.rb
@@ -1,2 +1,13 @@
+require 'chronic_duration'
+
 class RaceEntry < ActiveRecord::Base
+  
+  def elapsed_time
+    return ChronicDuration.output(@time)
+  end
+  
+  def set_elapsed_time(time_string)
+    @time = ChronicDuration.parse(time_string)
+  end
+  
 end
--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,7 +1,10 @@
+require 'uri'
+
 class Racer < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
   validates :gender, presence: true
+  # validates :birth_date, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :birth_date, presence: true
 end
