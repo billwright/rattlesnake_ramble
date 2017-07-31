af9ebe8e499993b2fc251b66c24dfdb2a3d7bef5
Bill Wright <bill@wwwright.com>
2017-07-31 04:06:15 +0000

    Added the create page for racer with validations and notifications"

--- app/controllers/racers_controller.rb
+++ app/controllers/racers_controller.rb
@@ -9,4 +9,25 @@ class RacersController < ApplicationController
     # binding.pry
   end
   
+  def new
+    @racer = Racer.new
+  end
+  
+  def create
+    # binding.pry
+    @racer = Racer.new(recipe_params)
+    
+    if @racer.save
+      flash[:success] = "Your racer was created successfully!"
+      redirect_to racers_path
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+    def recipe_params
+      params.require(:racer).permit(:first_name, :last_name, :email, :gender, :birth_date, :city, :state)
+    end
 end
