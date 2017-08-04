eaaeddf7588f997a132b4f782eea7ba9543f4f9e
moveson <mark.oveson@gmail.com>
2017-08-03 18:28:24 -0600

    Update ApplicationHelper.datepicker_input to function properly when editing an existing Racer.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,13 +10,14 @@ module ApplicationHelper
   def datepicker_input(args)
     form = args[:form]
     field = args[:field]
+    existing_date = args[:existing_date]
     default_date = args[:default_date]
     default_view = args[:default_view]
-    content_tag :td do
-      form.text_field field,
-                      class: 'datepicker',
-                      placeholder: 'mm/dd/yyyy',
-                      data: {provide: 'datepicker', date_default_view_date: default_date, date_start_view: default_view}
-    end
+    start_view = existing_date ? 'day' : default_view
+    form.text_field field,
+                    value: existing_date&.strftime('%m/%d/%Y'),
+                    class: 'datepicker',
+                    placeholder: 'mm/dd/yyyy',
+                    data: {provide: 'datepicker', date_default_view_date: default_date, date_start_view: start_view}
   end
 end
