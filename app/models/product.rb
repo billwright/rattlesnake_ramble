9751f6425f4aeb5406b7c31a8211bdb3a103a5bb
Bill Wright <bill@wwwright.com>
2017-08-16 21:02:14 +0000

    Added images for all merchandise and code to the seeds file to create it all in production.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  has_many :product_images
+  has_many :product_images, dependent: :destroy
   validates :description, presence: true
   validates :quantity, presence: true
   validates :price, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,21 +18,36 @@ kidsRace = races.last
 currentEdition = RaceEdition.create(race: currentRace, date: "2017-09-09", entry_fee: 25)
 kidsEdition = RaceEdition.create(race: kidsRace, date: "2017-09-09", entry_fee: 10)
 
+racer_bill = Racer.create(first_name: "Bill", last_name: "Wright", email: "bill@wwwright.com", gender: :male, birth_date: "1962-04-26", city: "Superior", state: "Colorado")
+racer_sheri = Racer.create(first_name: "Sheri", last_name: "Wright", email: "sheri_wright@oracle.com", gender: :female, birth_date: "1963-12-18", city: "Superior", state: "Colorado")
+racer_derek = Racer.create(first_name: "Derek", last_name: "Wright", email: "poochito@gmail.com", gender: :male, birth_date: "1998-01-29", city: "Superior", state: "Colorado")
+    
+RaceEntry.create(racer: racer_bill, race_edition: currentEdition, paid: true)
+RaceEntry.create(racer: racer_sheri, race_edition: currentEdition, paid: true)
+RaceEntry.create(racer: racer_derek, race_edition: currentEdition, paid: false)
+
+
+# 2017 Merchandise
+
+Product.all.each do |prod|
+    prod.destroy
+end
+
 prod = Product.create(description: "Men's Cotton 2017 Race Shirt, size medium", quantity: 10, price: 25)
-ProductImage.create(product: prod, url: 'RR2017MensShirtFront.png', alt_text: "Men's Race Shirt Front")
+ProductImage.create(product: prod, url: 'RR2017MensShirtFront.jpg', alt_text: "Men's Race Shirt Front")
 prod = Product.create(description: "Men's Cotton 2017 Race Shirt, size large", quantity: 10, price: 25)
-ProductImage.create(product: prod, url: 'RR2017MensShirtFront.png', alt_text: "Men's Race Shirt Front")
+ProductImage.create(product: prod, url: 'RR2017MensShirtFront.jpg', alt_text: "Men's Race Shirt Front")
 prod = Product.create(description: "Men's Cotton 2017 Race Shirt, size extra-large", quantity: 3, price: 25)
-ProductImage.create(product: prod, url: 'RR2017MensShirtFront.png', alt_text: "Men's Race Shirt Front")
+ProductImage.create(product: prod, url: 'RR2017MensShirtFront.jpg', alt_text: "Men's Race Shirt Front")
 
 prod = Product.create(description: "Women's Cotton 2017 Race Shirt, size small", quantity: 10, price: 25)
-ProductImage.create(product: prod, url: 'RR2017WomensShirtFront.png', alt_text: "Women's Race Shirt Front")
+ProductImage.create(product: prod, url: 'RR2017WomensShirtFront.jpg', alt_text: "Women's Race Shirt Front")
 ProductImage.create(product: prod, url: 'RR2017WomensShirtBack.png', alt_text: "Women's Race Shirt Back")
 prod = Product.create(description: "Women's Cotton 2017 Race Shirt, size medium", quantity: 10, price: 25)
-ProductImage.create(product: prod, url: 'RR2017WomensShirtFront.png', alt_text: "Women's Race Shirt Front")
+ProductImage.create(product: prod, url: 'RR2017WomensShirtFront.jpg', alt_text: "Women's Race Shirt Front")
 ProductImage.create(product: prod, url: 'RR2017WomensShirtBack.png', alt_text: "Women's Race Shirt Back")
 prod = Product.create(description: "Women's Cotton 2017 Race Shirt, size large", quantity: 5, price: 25)
-ProductImage.create(product: prod, url: 'RR2017WomensShirtFront.png', alt_text: "Women's Race Shirt Front")
+ProductImage.create(product: prod, url: 'RR2017WomensShirtFront.jpg', alt_text: "Women's Race Shirt Front")
 ProductImage.create(product: prod, url: 'RR2017WomensShirtBack.png', alt_text: "Women's Race Shirt Back")
 
 prod = Product.create(description: "Men's Cotton 2017 Race Shirt, size medium", quantity: 10, price: 25)
@@ -53,18 +68,11 @@ ProductImage.create(product: prod, url: 'RRMainLogoWomensShirtFront.png', alt_te
 ProductImage.create(product: prod, url: 'RRMainLogoWomensShirtBack.png', alt_text: "Women's Race Shirt Back")
 
 prod = Product.create(description: "Black, Cotton, Hooded, Uni-Sex Sweatshirt, size small", quantity: 5, price: 40)
-ProductImage.create(product: prod, url: 'SweatshirtMainLogoFront.png', alt_text: "Sweatshirt Front")
+ProductImage.create(product: prod, url: 'RattlesnakeRambleHoodie.png', alt_text: "Sweatshirt Front")
 prod = Product.create(description: "Black, Cotton, Hooded, Uni-Sex Sweatshirt, size medium", quantity: 5, price: 40)
-ProductImage.create(product: prod, url: 'SweatshirtMainLogoFront.png', alt_text: "Sweatshirt Front")
+ProductImage.create(product: prod, url: 'RattlesnakeRambleHoodie.png', alt_text: "Sweatshirt Front")
 prod = Product.create(description: "Black, Cotton, Hooded, Uni-Sex Sweatshirt, size large", quantity: 5, price: 40)
-ProductImage.create(product: prod, url: 'SweatshirtMainLogoFront.png', alt_text: "Sweatshirt Front")
+ProductImage.create(product: prod, url: 'RattlesnakeRambleHoodie.png', alt_text: "Sweatshirt Front")
 prod = Product.create(description: "Black, Cotton, Hooded, Uni-Sex Sweatshirt, size extra-large", quantity: 3, price: 40)
-ProductImage.create(product: prod, url: 'SweatshirtMainLogoFront.png', alt_text: "Sweatshirt Front")
+ProductImage.create(product: prod, url: 'RattlesnakeRambleHoodie.png', alt_text: "Sweatshirt Front")
   
-racer_bill = Racer.create(first_name: "Bill", last_name: "Wright", email: "bill@wwwright.com", gender: :male, birth_date: "1962-04-26", city: "Superior", state: "Colorado")
-racer_sheri = Racer.create(first_name: "Sheri", last_name: "Wright", email: "sheri_wright@oracle.com", gender: :female, birth_date: "1963-12-18", city: "Superior", state: "Colorado")
-racer_derek = Racer.create(first_name: "Derek", last_name: "Wright", email: "poochito@gmail.com", gender: :male, birth_date: "1998-01-29", city: "Superior", state: "Colorado")
-    
-RaceEntry.create(racer: racer_bill, race_edition: currentEdition, paid: true)
-RaceEntry.create(racer: racer_sheri, race_edition: currentEdition, paid: true)
-RaceEntry.create(racer: racer_derek, race_edition: currentEdition, paid: false)
