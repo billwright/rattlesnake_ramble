609869b918f7185d7ebe632e8860e7140cd94c4c
moveson <mark.oveson@gmail.com>
2017-09-06 10:12:41 -0600

    Add friendly_id gem; add friendly find to RacesController and RaceEditionsController; incorporate friendly_id into Race model and RaceEdition model; add associated validations and tests.

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,4 +1,8 @@
 class Race < ActiveRecord::Base
+  extend FriendlyId
+
   has_many :race_editions
   validates :name, presence: true, length: { minimum: 5, maximum: 100 }
+  validates_uniqueness_of :name, case_sensitive: false
+  friendly_id :name, use: :slugged
 end
