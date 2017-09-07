3e424520b141fbb981edb818593a18fe875f3bca
moveson <mark.oveson@gmail.com>
2017-09-07 12:38:44 -0600

    Rename OST environment variables.

--- app/services/ost/get_token.rb
+++ app/services/ost/get_token.rb
@@ -2,8 +2,8 @@ module OST
   class GetToken
     def self.perform
       full_url = "#{ENV['OST_URL']}#{OST::Constants::AUTH_ENDPOINT}"
-      response = RestClient.post(full_url, {user: {email: ENV['OST_RR_EMAIL'],
-                                                   password: ENV['OST_RR_PASSWORD']}})
+      response = RestClient.post(full_url, {user: {email: ENV['OST_EMAIL'],
+                                                   password: ENV['OST_PASSWORD']}})
       OST::Response.new(response)
 
     rescue RestClient::ExceptionWithResponse => e
