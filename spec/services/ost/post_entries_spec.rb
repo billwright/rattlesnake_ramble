4705926b90e592774da7714593d325a0205bb76f
Mark Oveson <mark.oveson@mx.com>
2020-09-03 22:33:21 -0600

    Make expected URL more flexible in post entries spec

--- spec/services/ost/post_entries_spec.rb
+++ spec/services/ost/post_entries_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe OST::PostEntries do
     it 'sends a properly constructed URL to RestClient' do
       allow(RestClient).to receive(:post)
       OST::PostEntries.perform(race_edition: race_edition, ost_event_id: 'test-event', token: token)
-      expected_url = 'https://ost-stage.herokuapp.com/api/v1/events/test-event/import'
+      expected_url = "#{ENV['OST_URL']}/api/v1/events/test-event/import"
       expect(RestClient).to have_received(:post).with(expected_url, anything, anything)
     end
 
