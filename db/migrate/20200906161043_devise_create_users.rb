65974a8bb942ee4c927a1f13ad26da5d28e89c28
Mark Oveson <mark.oveson@mx.com>
2020-09-06 14:28:39 -0600

    Remove index from users table

--- db/migrate/20200906161043_devise_create_users.rb
+++ db/migrate/20200906161043_devise_create_users.rb
@@ -9,7 +9,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    add_index :users, :email, unique: true
   end
 end
