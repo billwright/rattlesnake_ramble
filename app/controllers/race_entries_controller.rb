c1cbcfbda27da301a8f5352ab1d3fc2ca280e4ce
Mark Oveson <mark.oveson@mx.com>
2020-09-06 13:17:33 -0600

    Remove unused views and actions

--- app/controllers/race_entries_controller.rb
+++ app/controllers/race_entries_controller.rb
@@ -1,37 +1,17 @@
 class RaceEntriesController < ApplicationController
-  def index
-    sort = params[:sort] || 'racers.last_name'
-    @race_entries = RaceEntry.includes(:racer, :race_edition).order(sort).map { |re| RaceEntryPresenter.new(re) }
-  end
-  
   def show
     @race_entry = RaceEntryPresenter.new(RaceEntry.find(params[:id]))
   end
   
-  def new
-    @race_entry = RaceEntry.new
-  end
-  
-  def create
-    @race_entry = RaceEntry.new(obj_params)
-    
-    if @race_entry.save
-      flash[:success] = "Your entry was created successfully!"
-      redirect_to race_entries_path
-    else
-      render :new
-    end
-  end
-  
   def edit
-    @race_entry = RaceEntry.find(params[:id])
+    @race_entry = RaceEntryPresenter.new(RaceEntry.find(params[:id]))
   end
   
   def update
     @race_entry = RaceEntry.find(params[:id])
     if @race_entry.update(obj_params)
       flash[:success] = "Your race entry was updated successfully"
-      redirect_to race_entries_path
+      redirect_to race_entries_race_edition_path(@race_entry.race_edition)
     end
   end
 
@@ -39,7 +19,7 @@ class RaceEntriesController < ApplicationController
     @race_entry = RaceEntry.find(params[:id])
     if @race_entry.destroy
       flash[:success] = "Your race entry was deleted"
-      redirect_to race_entries_path
+      redirect_to race_entries_race_edition_path(@race_entry.race_edition)
     end
   end
   
@@ -54,7 +34,7 @@ class RaceEntriesController < ApplicationController
   
   def cancelled_payment
     race_entry = RaceEntry.find(params[:id])
-    flash[:success] = "Until you pay, you are not officially in the Rattlesnake Ramble. Please pay via PayPal prmoptly or contact the race director (bwright@rattlesnakeramble.org)."
+    flash[:success] = "Until you pay, you are not officially in the Rattlesnake Ramble. Please pay via PayPal promptly or contact the race director (bwright@rattlesnakeramble.org)."
     redirect_to race_edition_path(race_entry.race_edition)
   end
   
