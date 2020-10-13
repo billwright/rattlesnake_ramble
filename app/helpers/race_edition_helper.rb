0a0ed1e44c9b43917638d22de04b226c5f4a7df2
Mark Oveson <mark.oveson@mx.com>
2020-10-12 23:51:23 -0600

    Make home page fully dynamic

--- app/helpers/race_edition_helper.rb
+++ app/helpers/race_edition_helper.rb
@@ -1,9 +1,17 @@
-class RaceEditionHelper
+module RaceEditionHelper
   def current_full_course_edition
-    RaceEdition.full_course.order(date: :desc).first
+    @current_full_course_edition ||=
+        begin
+          race_edition = RaceEdition.full_course.order(date: :desc).first
+          RaceEditionPresenter.new(race_edition)
+        end
   end
 
   def current_kids_course_edition
-    RaceEdition.kids_race.order(date: :desc).first
+    @current_kids_course_edition ||=
+        begin
+          race_edition = RaceEdition.kids_race.order(date: :desc).first
+          RaceEditionPresenter.new(race_edition)
+        end
   end
 end
