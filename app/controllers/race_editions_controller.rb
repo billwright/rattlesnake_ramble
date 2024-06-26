c1434800eeeea9f47460f3c6f4c9e05a9956df7b
Bill Wright <bill@wwwright.com>
2024-06-25 22:19:17 -0600

    Fixes after testing with UI, but still have some issues, like tests not passing.

--- app/controllers/race_editions_controller.rb
+++ app/controllers/race_editions_controller.rb
@@ -123,7 +123,7 @@ class RaceEditionsController < ApplicationController
 
   def obj_params
     params.require(:race_edition)
-        .permit(:race_id, :date, :entry_fee, :default_start_time_male_local, :default_start_time_female_local, :accepting_entries,
+        .permit(:race_id, :date, :entry_fee, :default_start_time_male_local, :default_start_time_female_local, :accepting_entries, :selling_merchandise, :merchandise_description, :merchandise_image_file_name, :merchandise_price,
                 racers_attributes: [:id, :first_name, :last_name, :email, :gender, :birth_date, :city, :state],
                 race_entries_attributes: [:elapsed_predicted_time, :merchandise_size])
   end
