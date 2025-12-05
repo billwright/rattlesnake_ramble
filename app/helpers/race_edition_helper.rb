def3bac258543ca6cd9e1725e9f7b13670ed655c
Sri venkatesha mani <srivenkateshmani@gmail.com>
2025-12-05 14:29:40 -0700

    Allow PayPal return without auth; pick next upcoming editions

--- app/helpers/race_edition_helper.rb
+++ app/helpers/race_edition_helper.rb
@@ -1,18 +1,18 @@
 module RaceEditionHelper
   def current_full_course_edition
-    @current_full_course_edition ||=
-        begin
-          race_edition = RaceEdition.full_course.order(date: :desc).first
-          RaceEditionPresenter.new(race_edition)
-        end
+    @current_full_course_edition ||= begin
+      race_edition = RaceEdition.full_course.where("date >= ?", Date.today).order(date: :asc).first
+      race_edition ||= RaceEdition.full_course.order(date: :desc).first
+      RaceEditionPresenter.new(race_edition)
+    end
   end
 
   def current_kids_course_edition
-    @current_kids_course_edition ||=
-        begin
-          race_edition = RaceEdition.kids_race.order(date: :desc).first
-          RaceEditionPresenter.new(race_edition)
-        end
+    @current_kids_course_edition ||= begin
+      race_edition = RaceEdition.kids_race.where("date >= ?", Date.today).order(date: :asc).first
+      race_edition ||= RaceEdition.kids_race.order(date: :desc).first
+      RaceEditionPresenter.new(race_edition)
+    end
   end
 
   def race_editions_not_aligned?
